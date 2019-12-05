c8a10a661261446170660ac88a05a46d8ede21a5
JamiyyatulMunawwaroh <57267157+JamiyyatulMunawwaroh@users.noreply.github.com>
2019-12-05 13:37:49 +0700

    Initial commit

---
+++ app/src/main/java/com/jamiyyatulmunawwaroh/gabunglayout/gabunglayout_2.java
@@ -0,0 +1,13 @@
+package com.jamiyyatulmunawwaroh.gabunglayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gabunglayout_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gabunglayout_2);
+    }
+}
